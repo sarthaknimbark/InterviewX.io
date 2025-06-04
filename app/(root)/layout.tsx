c8dd4f2a551041baba577aa03e16d3f945939ab2
Sarthak Nimbark <sarthaknimbark13@gmail.com>
2025-06-04 15:50:23 +0530

    Code Refactored

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
 import { ReactNode } from 'react'
 
 const Rootlayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div className='root-layout'>
+      <nav> 
+        <Link 
+          href="/"
+          className="flex items-center gap-2"
+        >
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h1 className="text-primary-100">InterviewX.io</h1>
+        </Link>
+      </nav>
       {children}
     </div>
   )
